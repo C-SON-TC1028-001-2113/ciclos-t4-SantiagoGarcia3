6dc3a32252557e0cf7e74721f8db0f5f364d58fa
SantiagoGarcia3 <A01252741@itesm.mx>
2021-09-06 22:40:43 +0000

    Tarea4 Santiago

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,16 @@
-
 def main():
-
-
-if __name__=='__main__':
-    main()
+    #escribe tu codigo abajo de esta linea
+    nu1 = int(input("Valor 1: "))
+    nu2 = int(input("Valor 2: "))
+    if nu1>0 and nu2>0 :
+        if nu1!=nu2:
+            if nu1>nu2:
+                for i in range(nu2, nu1+1):
+                    if i%2==0 :
+                        print(i)
+            elif nu2>nu1 :
+                for i in range(nu1, nu2+1):
+                    if i%2==0 :
+                        print(i)
+        else:
+            print('No hay pares')
